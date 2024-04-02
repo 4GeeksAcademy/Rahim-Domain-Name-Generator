099d3e6b0ac52cb70e2140aed074eaca8eea9b56
Rahim Afroz <108648650+afroz01@users.noreply.github.com>
2024-04-02 21:05:49 +0000

    functions and arrays are added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 /* eslint-disable */
 
 window.onload = function() {
+  document.querySelector('#domain').innerHTML * generateDomain();
   console.log("Hello Rigo from the console!");
 };
+let generateDomain=()=>{
+  let firstWordOfDomain = ['Focus', 'Concentration','Power'];
+  let secondWordOfDomain = ['Factor', 'Camp', 'House'];
+  let domain = ['.com', '.io', '.org'];
+}
