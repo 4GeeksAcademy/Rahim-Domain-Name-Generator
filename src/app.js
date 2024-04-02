0a8d516a033271e476ff9512214d8410be7e54d8
Rahim Afroz <108648650+afroz01@users.noreply.github.com>
2024-04-02 21:35:46 +0000

    innerHTML fixed

--- src/app.js
+++ src/app.js
@@ -1,21 +1,25 @@
 /* eslint-disable */
 
 window.onload = function() {
-  document.querySelector('#domain').innerHTML * generateDomain();
+  document.querySelector("#domain").innerHTML = generateDomain();
   console.log("Hello Rigo from the console!");
 };
 //declaring function
-let generateDomain=()=>{
-  let firstWordOfDomain = ['Focus', 'Concentration','Power'];
-  let secondWordOfDomain = ['Factor', 'Camp', 'House'];
-  let domain = ['.com', '.io', '.org'];
+let generateDomain = () => {
+  let firstWordOfDomain = ["Focus", "Concentration", "Power"];
+  let secondWordOfDomain = ["Factor", "Camp", "House"];
+  let domain = [".com", ".io", ".org"];
 
   //declaring the randomIndex
   let firstIndx = [Math.floor(Math.random() * firstWordOfDomain.length)];
   let secondIndx = [Math.floor(Math.random() * secondWordOfDomain.length)];
   let domainIndx = [Math.floor(Math.random() * domain.length)];
 
-  return firstWordOfDomain[firstIndx]+" "+secondWordOfDomain[secondIndx]+" "+domain[domainIndx];
-
-
-}
+  return (
+    firstWordOfDomain[firstIndx] +
+    " " +
+    secondWordOfDomain[secondIndx] +
+    " " +
+    domain[domainIndx]
+  );
+};
