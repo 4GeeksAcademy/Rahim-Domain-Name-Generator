ec25f6a8f457bda69682ad8f1b04380edfc4d05b
Rahim Afroz <108648650+afroz01@users.noreply.github.com>
2024-04-02 21:15:22 +0000

    random index added

--- src/app.js
+++ src/app.js
@@ -4,8 +4,16 @@ window.onload = function() {
   document.querySelector('#domain').innerHTML * generateDomain();
   console.log("Hello Rigo from the console!");
 };
+//declaring function
 let generateDomain=()=>{
   let firstWordOfDomain = ['Focus', 'Concentration','Power'];
   let secondWordOfDomain = ['Factor', 'Camp', 'House'];
   let domain = ['.com', '.io', '.org'];
+
+  //declaring the randomIndex
+  let firstIndx = [Math.floor(Math.random() * firstWordOfDomain.length)];
+  let secondIndx = [Math.floor(Math.random() * secondWordOfDomain.length)];
+  let domainIndx = [Math.floor(Math.random() * domain.length)];
+
+
 }
