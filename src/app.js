1b026f21580a76bd53befc99029e35659f6e972f
Rahim Afroz <108648650+afroz01@users.noreply.github.com>
2024-04-03 23:36:06 +0000

    loop dded

--- src/app.js
+++ src/app.js
@@ -2,28 +2,37 @@
 
 window.onload = function() {
   document.querySelector("#domain").innerHTML = generateDomain();
-  let i = Math.random;
-  for (i = 0; i < 4; ) {
+  let loopCombination = "";
+  for (let i = 0; i < firstWordOfDomain.length; i++) {
+    for (let a = 0; a < secondWordOfDomain.length; a++) {
+      for (let b = 0; b < domain.length; b++) {
+        loopCombination =
+          firstWordOfDomain[i] + " " + secondWordOfDomain[a] + " " + domain[b];
+      }
+    }
+
     console.log(i);
   }
-};
 
-//declaring function
-let generateDomain = () => {
-  let firstWordOfDomain = ["Focus", "Concentration", "Power"];
-  let secondWordOfDomain = ["Factor", "Camp", "House"];
-  let domain = [".com", ".io", ".org"];
+  //declaring function
+  let generateDomain = () => {
+    let firstWordOfDomain = ["Focus", "Concentration", "Power"];
+    let secondWordOfDomain = ["Factor", "Camp", "House"];
+    let domain = [".com", ".io", ".org"];
 
-  //declaring the randomIndex
-  let firstIndx = [Math.floor(Math.random() * firstWordOfDomain.length)];
-  let secondIndx = [Math.floor(Math.random() * secondWordOfDomain.length)];
-  let domainIndx = [Math.floor(Math.random() * domain.length)];
+    //declaring the randomIndex
+    let firstIndx = [Math.floor(Math.random() * firstWordOfDomain.length)];
+    let secondIndx = [Math.floor(Math.random() * secondWordOfDomain.length)];
+    let domainIndx = [Math.floor(Math.random() * domain.length)];
 
-  let random =
-    firstWordOfDomain[firstIndx] +
-    " " +
-    secondWordOfDomain[secondIndx] +
-    " " +
-    domain[domainIndx];
-  return random;
+    let random =
+      firstWordOfDomain[firstIndx] +
+      " " +
+      secondWordOfDomain[secondIndx] +
+      " " +
+      domain[domainIndx];
+
+    let continuous = random;
+    return random;
+  };
 };
