cde64ad85f21cf4da7baa9ade1fe3837c5d959eb
Rahim Afroz <108648650+afroz01@users.noreply.github.com>
2024-04-04 20:29:24 +0000

    continuous loop added

--- src/app.js
+++ src/app.js
@@ -1,38 +1,44 @@
-/* eslint-disable */
+let firstWordOfDomain = ["Focus", "Concentration", "Power"];
+let secondWordOfDomain = ["Factor", "Camp", "House"];
+let domain = [".com", ".io", ".org"];
 
-window.onload = function() {
-  document.querySelector("#domain").innerHTML = generateDomain();
+let generateDomain = () => {
+  let firstIndx = Math.floor(Math.random() * firstWordOfDomain.length);
+  let secondIndx = Math.floor(Math.random() * secondWordOfDomain.length);
+  let domainIndx = Math.floor(Math.random() * domain.length);
+
+  let random =
+    firstWordOfDomain[firstIndx] +
+    " " +
+    secondWordOfDomain[secondIndx] +
+    " " +
+    domain[domainIndx];
+
+  return random;
+};
+
+let generateCombinations = () => {
   let loopCombination = "";
   for (let i = 0; i < firstWordOfDomain.length; i++) {
     for (let a = 0; a < secondWordOfDomain.length; a++) {
       for (let b = 0; b < domain.length; b++) {
-        loopCombination =
-          firstWordOfDomain[i] + " " + secondWordOfDomain[a] + " " + domain[b];
+        loopCombination +=
+          firstWordOfDomain[i] +
+          " " +
+          secondWordOfDomain[a] +
+          " " +
+          domain[b] +
+          "<br>";
       }
     }
-
-    console.log(i);
   }
+  //console.log(loopCombination);
+};
 
-  //declaring function
-  let generateDomain = () => {
-    let firstWordOfDomain = ["Focus", "Concentration", "Power"];
-    let secondWordOfDomain = ["Factor", "Camp", "House"];
-    let domain = [".com", ".io", ".org"];
-
-    //declaring the randomIndex
-    let firstIndx = [Math.floor(Math.random() * firstWordOfDomain.length)];
-    let secondIndx = [Math.floor(Math.random() * secondWordOfDomain.length)];
-    let domainIndx = [Math.floor(Math.random() * domain.length)];
-
-    let random =
-      firstWordOfDomain[firstIndx] +
-      " " +
-      secondWordOfDomain[secondIndx] +
-      " " +
-      domain[domainIndx];
-
-    let continuous = random;
-    return random;
-  };
+window.onload = function() {
+  document.querySelector("#domain").innerHTML = generateDomain();
+  setInterval(() => {
+    document.querySelector("#domain").innerHTML = generateDomain();
+    generateCombinations();
+  }, 5000);
 };
